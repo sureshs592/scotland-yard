68750fa5e3ca514ff4212ff74aee58d56caf22e7
Suresh Subramaniam <sureshs.592@gmail.com>
2014-06-18 15:36:58 +0530

    - Experimenting with D3 - Added jQuery package

--- scotland-yard.js
+++ scotland-yard.js
@@ -1,19 +1,13 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to scotland-yard.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  drawCanvas();
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
   });
+}
+
+function drawCanvas() {
+  
 }
