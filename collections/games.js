d78de062c1032e0f7bf5a7b1973762564ec3c1cf
Suresh Subramaniam <sureshs.592@gmail.com>
2014-06-21 13:07:20 +0530

    Added _id attribute to Move schema. Added collection to store Games.

---
+++ collections/games.js
@@ -0,0 +1,9 @@
+/*
+Document Schema:
+{
+  _id: ###,
+  name: Round-1
+}
+*/
+
+Games = new Meteor.Collection('games');
--- collections/moves.js
+++ collections/moves.js
@@ -1,6 +1,7 @@
 /*
 Document Schema:
 {
+  _id: ###,
   game: (_id of game object),
   player: thief/detective-1/2/3/4/5,
   station: 192,
