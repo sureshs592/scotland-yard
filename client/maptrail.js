1b3249eba38bd41912017228d1b49d2f95f6c7a8
Suresh Subramaniam <sureshs.592@gmail.com>
2014-06-21 19:36:47 +0530

    Showing the entries from the Moves collection

--- client/maptrail.js
+++ client/maptrail.js
@@ -1,5 +1,11 @@
 Template.mapTrail.helpers({
   stations: function() {
-    return Moves.find();
+    return Moves.find().fetch();
   }
 });
+
+Template.station.helpers({
+  moveTime: function() {
+    return this.time.getHours() + ":" + this.time.getMinutes();
+  }
+})
