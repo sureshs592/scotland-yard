5c6fdd4a82d54dfe218e4b0e93a7d1cf2ea0e74f
Suresh Subramaniam <sureshs.592@gmail.com>
2014-06-21 23:45:51 +0530

    Forcing manual render of player trail. Works now.

--- client/main.js
+++ client/main.js
@@ -2,14 +2,28 @@
 // var games = Games.find({}).fetch();
 // var gameID = games[0]._id;
 //END OF TEMP CODE
+Template.mapTrail.helpers({
+  stations: function() {
+    return Moves.find().fetch();
+  }
+});
+
+Template.station.helpers({
+  moveTime: function() {
+    return this.time.getHours() + ":" + this.time.getMinutes();
+  }
+});
+
+//CONSTANTS
+var stationGap = 40;
 
 $(document).ready(function() {
   //Render map once the page has loaded
-  renderMapTrail("thief");
+  // renderMapTrail("thief");
 
   //Trigger form to save a move
   $("#move-form-submit").click(function (e) {
-    //Assign form variables
+    // Assign form variables
     var button = e.target;
 
     //Disable button and show message
@@ -25,76 +39,99 @@ $(document).ready(function() {
     $(button).removeAttr("disabled");
     $(button).text("Save");
   });
-});
 
-/*
-* Function to save the move of a player
-*/
-function saveMove(playerType, stationNumber, transport) {
-  //Checking if the station number is valid
-  if (!stationNumber || isNaN(stationNumber)) {
-    console.log("Station is not a number!");
+  $("#test").click(function (e) {
+    renderMapTrail("thief");
+  });
+
+  /*
+  * Function to save the move of a player
+  */
+  function saveMove(playerType, stationNumber, transport) {
+    //Checking if the station number is valid
+    if (!stationNumber || isNaN(stationNumber)) {
+      console.log("Station is not a number!");
+      return;
+    }
+
+    var games = Games.find({}).fetch();
+    var gameID = games[0]._id;
+
+    var lastMove = Moves.findOne({"game": gameID}, {sort: [["order", "desc"]]});
+    var moveOrder = (lastMove) ? lastMove.order + 1 : 1 ;
+
+    //Creating new move document and storing it
+    var newMove = {
+      game: gameID,
+      order: moveOrder,
+      time: new Date(),
+      player: playerType,
+      station: stationNumber,
+      transport: transport,
+    };
+
+    Moves.insert(newMove);
+  }
+
+  function renderMapTrail(player) {
+    console.log("Rendering map trail");
+    var canvasID = "#" + player + "-canvas";
+    var divID = "#" + player + "-canvas-div";
+    var canvas = $(canvasID);
+    var stations = new Array();
+    var paths = new Array();
+
+    //Get all moves made by the player
+    var games = Games.find({}).fetch();
+    var gameID = games[0]._id;
+    var moves = Moves.find({"game": gameID}).fetch();
+    console.log(moves);
+
+    //Starting point of the trail
+    var cx = 10, cy = 10;
+    var startingPoint = createStation("s_0", "station", cx, cy, 6);
+    stations.push(startingPoint);
+
+    //Prepare paths and stations for subsequent moves
+    for (var i = 0; i < moves.length; i++) {
+      var move = moves[i];
+      var idAppend = "_" + (i + 1);
+      var clazz = "path " + move.transport;
+      var path = createTrip("p" + idAppend, clazz, cx, cy);
+      cy += stationGap;
+      var station = createStation("s" + idAppend, "station", cx, cy, 6);
+
+      paths.push(path);
+      stations.push(station);
+    }
+
+    //Render all paths and stations
+    for (var i = 0; i < paths.length; i++) { $(canvas).append(paths[i]); }
+    for (var i = 0; i < stations.length; i++) { $(canvas).append(stations[i]); }
+
+    //Hack to make the inserted SVG visible
+    $(divID).html($(divID).html());
     return;
   }
 
-  var games = Games.find({}).fetch();
-  var gameID = games[0]._id;
-
-  var lastMove = Moves.findOne({"game": gameID}, {sort: [["order", "desc"]]});
-  var moveOrder = (lastMove) ? lastMove.order + 1 : 1 ;
-
-  //Creating new move document and storing it
-  var newMove = {
-    game: gameID,
-    order: moveOrder,
-    time: new Date(),
-    player: playerType,
-    station: stationNumber,
-    transport: transport,
-  };
-
-  Moves.insert(newMove);
-}
-//<circle cx="10" cy="10" r="6" class="station" id="s_1"></circle>
-function renderMapTrail(player) {
-  console.log("Rendering map trail");
-  var canvasID = "#" + player + "-canvas";
-  var divID = "#" + player + "-canvas-div";
-  var canvas = $(canvasID);
-  console.log(canvas);
-
-  //Starting point of the trail
-  var startingPoint = createStation("s_0", "station", 10, 10, 6);
-  $(canvas).append(startingPoint);
-
-  //Start rendering subsequent moves
-  var games = Games.find({}).fetch();
-  var gameID = games[0]._id;
-  var moves = Moves.find({"game": gameID}).fetch();
-  console.log();
-
-  //Hack to make the inserted SVG visible
-  $(divID).html($(divID).html());
-}
-
-function createStation(id, clazz, cx, cy, r) {
-  var circle = document.createElement("circle");
-  $(circle).addClass(clazz);
-  $(circle).attr("id", id);
-  $(circle).attr("cx", cx);
-  $(circle).attr("cy", cy);
-  $(circle).attr("r", r);
-  return circle;
-}
-
-Template.mapTrail.helpers({
-  stations: function() {
-    return Moves.find().fetch();
+  function createStation(id, clazz, cx, cy, r) {
+    var circle = document.createElement("circle");
+    $(circle).addClass(clazz);
+    $(circle).attr("id", id);
+    $(circle).attr("cx", cx);
+    $(circle).attr("cy", cy);
+    $(circle).attr("r", r);
+    return circle;
   }
-});
 
-Template.station.helpers({
-  moveTime: function() {
-    return this.time.getHours() + ":" + this.time.getMinutes();
+  function createTrip(id, clazz, x1, y1) {
+    var path = document.createElement("line");
+    $(path).addClass(clazz);
+    $(path).attr("id", id);
+    $(path).attr("x1", x1);
+    $(path).attr("y1", y1);
+    $(path).attr("x2", x1);
+    $(path).attr("y2", y1 + stationGap);
+    return path;
   }
 });
